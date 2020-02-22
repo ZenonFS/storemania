14a2ffa0eeafe49d662c51b79ee8d22d16464932
jhoan12 <jhoanalexis1229@gmail.com>
2020-02-22 18:41:31 -0500

    Eso no se necesitaba.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 //bootstrap
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //rutas
